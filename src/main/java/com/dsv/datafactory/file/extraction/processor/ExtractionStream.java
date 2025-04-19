bf74e21d7fc92422af53bcd4fe0f4fbd86f52d5a
MaciejKamianowski <maciek-kamianowski@o2.pl>
2025-04-19 17:26:45 +0200

    REVIEW - comment 6 - ExtractionStream

--- src/main/java/com/dsv/datafactory/file/extraction/processor/ExtractionStream.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/ExtractionStream.java
@@ -1,4 +1,5 @@
 package com.dsv.datafactory.file.extraction.processor;
+// add empty new line between package name and imports
 import com.dsv.datafactory.model.MetaData;
 import com.dsv.datafactory.serde.MetaDataSerde;
 import com.google.inject.Inject;
@@ -13,6 +14,8 @@ import org.apache.kafka.streams.KeyValue;
 
 public class ExtractionStream {
 
+	// those two fields could be final
+	// if we are not going to reassign them
 	private Config config;
 
 	private ExtractContent extractDocument;
@@ -29,10 +32,36 @@ public class ExtractionStream {
 		KStream<String, MetaData> stream = builder.stream(config.imageExtractionMetadataTopic,
 				Consumed.with(Serdes.String(), new MetaDataSerde()));
       
-      
+      	// no error logging or handling
+		// what if extractDocument.execute() will throw an exception ?
+
 		KStream<String, MetaData> documentExtractions = stream.mapValues(extractDocument::execute);
+		// use method references constistently
+		// .filter((k, v) -> Objects.nonNull(v))
 		KStream<String, MetaData> documentExtractionsFiltered = documentExtractions.filter((k, v) -> v != null);
 		documentExtractionsFiltered.to(config.extractedDocumentTopic, Produced.with(Serdes.String(), new MetaDataSerde()));
+
+		// my proposal for code
+/*		KStream<String, MetaData> stream = builder.stream(
+				config.imageExtractionMetadataTopic,
+				Consumed.with(Serdes.String(), new MetaDataSerde())
+		);
+
+		KStream<String, MetaData> documentExtractions = stream.mapValues(value -> {
+			try {
+				return extractDocument.execute(value);
+			} catch (Exception e) {
+				// TODO: Replace with real logger
+				System.err.println("Failed to extract: " + e.getMessage());
+				return null;
+			}
+		});
+
+		documentExtractions
+				.filter((k, v) -> v != null)
+				.to(config.extractedDocumentTopic, Produced.with(Serdes.String(), new MetaDataSerde()));*/
+
+
 	}
 
 }

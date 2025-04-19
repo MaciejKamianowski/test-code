f079a89d4b236565ba98c609ec4bc7191ac0af7d
MaciejKamianowski <maciek-kamianowski@o2.pl>
2025-04-19 18:27:33 +0200

    REVIEW - comment 12 - PackageLoggersConfig

--- src/main/java/com/dsv/datafactory/file/extraction/processor/logging/PackageLoggersConfig.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/logging/PackageLoggersConfig.java
@@ -5,6 +5,9 @@ import com.dsv.logger.ECSLogger;
 import static com.dsv.datafactory.file.extraction.processor.util.ConfigurationLoader.getOrDefault;
 
 public class PackageLoggersConfig {
+    // extract environment variable names as constants
+    // private static final String ENV_KAFKA_LOG_LEVEL = "KAFKA_LOG_LEVEL";
+    // private static final String ENV_LOG_LEVEL = "LOG_LEVEL";
     public static void configure(){
         configureKafkaLogger();
         configurePdfBoxLogger();
@@ -18,6 +21,7 @@ public class PackageLoggersConfig {
     }
 
     private static void configurePdfBoxLogger() {
+        // Uses constants for package names.
         String PDFBOX_LOGGER_NAME = "org.apache.pdfbox.pdmodel.font";
         ECSLogger.Level logLevel = ECSLogger.Level.valueOf(getOrDefault("LOG_LEVEL", ECSLogger.Level.WARN.name()));
 
@@ -27,6 +31,11 @@ public class PackageLoggersConfig {
     private static void configurePackageLogger(String packageName, ECSLogger.Level logLevel){
         ECSLogger logger = ECSLoggerProvider.getLogger(PackageLoggersConfig.class.getName());
         logger.setLevel(packageName, logLevel);
+
+        // Consider logging configuration at DEBUG level
+        // if (logger.isDebugEnabled()) {
+        //    logger.debug("Package {} log level set to {}", packageName, logLevel.name());
+        //}
         logger.info("Package " + packageName + " log level set to " + logLevel.name());
     }
 }

5f6fea8c123babe0c61dff90736bde8253e04663
MaciejKamianowski <maciek-kamianowski@o2.pl>
2025-04-19 18:46:46 +0200

    REVIEW - comment 14 - DocumentToDisk and ReadImage

--- src/main/java/com/dsv/datafactory/file/extraction/processor/domain/readers/ReadImage.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/domain/readers/ReadImage.java
@@ -14,23 +14,30 @@ import java.util.ArrayList;
 
 public class ReadImage {
 
-
+    // redundant new line
     private GoogleOcrP googleOcrP;
     private ExtractLines lineExtractor;
 
     @Inject
     public ReadImage(GoogleOcrP ocr, ExtractLines extractLines)
-    {
-        this.googleOcrP = ocr; this.lineExtractor= extractLines;
+    {   // formatting, opening curly bracket on the same line
+        this.googleOcrP = ocr; this.lineExtractor= extractLines;    // formatting:
+        // for better readability
+        // this.googleOcrP = ocr;
+        //    this.lineExtractor = extractLines;
     }
-
+    // replace ArrayList with List
+    // it makes more flexibility, meets PTI principle (program to interface)
     public Document extract(ArrayList<String> listOfPathImgs, String key ) throws Exception {
+       // clarify or remove this comment
         // made a new results each time to avoid accumulation
+        // better version
+        // Generate a fresh document for each request
         Document document = googleOcrP.generateDocument(listOfPathImgs,key);
         lineExtractor.generateInputFromDocument(document);
         return document;
 
     }
 
-
+    // redundant new line
 }
--- src/main/java/com/dsv/datafactory/file/extraction/processor/domain/saving/DocumentToDisk.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/domain/saving/DocumentToDisk.java
@@ -11,12 +11,18 @@ import java.nio.charset.StandardCharsets;
 
 
 public class DocumentToDisk {
-//    private String pathImages;
+//    private String pathImages;    // remove this
+    // wrong order
+    // should be static final or better
+    // private static final
 
-    final static Logger logger = Logger.getLogger(DocumentToDisk.class.getSimpleName());
-
-    public void execute(String key, Document res,String shipmentId) throws IOException
-    {
+    // introduce const
+    // private static final String DEFAULT_PATH_DOCUMENT = "/files/hocr/";
+    final static Logger logger = Logger.getLogger(DocumentToDisk.class.getSimpleName());    // const should be name LOGGER
+    //  final static Logger LOGGER = Logger.getLogger(DocumentToDisk.class);
+    public void execute(String key, Document res,String shipmentId) throws IOException  // missing space between command and String
+    {   // opening curly bracket should be on the same line
+        // extract const
         String pathDocument =  ConfigurationLoader.getOrDefault("PATH_DOCUMENT", "/files/hocr/")+shipmentId+"/";
         checkIfExists(pathDocument);
         saveDocumentAsJson(key, res, pathDocument);
@@ -24,7 +30,7 @@ public class DocumentToDisk {
 
     private void saveDocumentAsJson(String key, Document file,String pathToDocument) throws IOException
     {
-            writeToJson(key,file,pathToDocument);
+            writeToJson(key,file,pathToDocument);   // redundant tabula
     }
 
     private void checkIfExists(String pathShipmentIdFolder){
@@ -32,23 +38,40 @@ public class DocumentToDisk {
         if (!file.exists()) {
             file.mkdir();
         }
+        // use mkdirs() instead of mkdir() to support nested folders.
+        // log if creation has failed
+        //if (!file.exists() && !file.mkdirs()) {
+        //    LOGGER.warn("Could not create directory: " + file.getAbsolutePath());
+        //}
 
     }
-    private void writeToJson(String key, Document doc,String pathDocument) throws IOException {
-        String documentExt = "json";
-        String fullPath = pathDocument +key + "."+ documentExt;
+    private void writeToJson(String key, Document doc,String pathDocument) throws IOException { // missing space
+        String documentExt = "json";    // extarct this to const
+        String fullPath = pathDocument +key + "."+ documentExt; // missing space
         doc.setPathToDocumentFile(fullPath);
         logger.info("fullpath is: " + fullPath);
 
         File file = new File(fullPath);
-
+        // resource leak
         OutputStreamWriter writer =
                 new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8);
+        // use try with resources
+        // try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8)) {
+        //    ObjectMapper mapper = new ObjectMapper();
+        //    String jsonString = mapper.writeValueAsString(doc);
+        //    writer.write(jsonString);
+        //}
+        // extract ObjectMapper to configuration class as @Bean and inject it here
         ObjectMapper mapper = new ObjectMapper();
         String jsonString = mapper.writeValueAsString(doc);
         writer.write(jsonString);
         writer.close();
     }
 
+    // in configuration class
+/*    @Bean
+    public ObjectMapper mapper() {
+        return new ObjectMapper();
+    }*/
 
 }

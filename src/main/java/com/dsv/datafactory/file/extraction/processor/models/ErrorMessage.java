b427492b3b665afb6cff81e001d79b66d74e6794
MaciejKamianowski <maciek-kamianowski@o2.pl>
2025-04-19 18:05:49 +0200

    REVIEW - comment 10 - ErrorMessage

--- src/main/java/com/dsv/datafactory/file/extraction/processor/models/ErrorMessage.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/models/ErrorMessage.java
@@ -8,10 +8,52 @@ import lombok.Getter;
 
 import java.io.Serializable;
 
+// consider using @Data instead of @Getter, @Setter, and @RequiredArgsConstructor
+
 @RequiredArgsConstructor
 public class ErrorMessage implements Serializable
-{
+{   // opening curly bracket should be on the same line
+
+    // Add serialVersionUID
+
+    //  explicitly declare serialVersionUID when implementing Serializable to avoid issues during deserialization.
+    // private static final long serialVersionUID = 1L;
     @Getter @Setter @NonNull private String topicKey;
+    // mixing field-level and class-level annotations
+    // the @Getter and @Setter
+
     @Getter @Setter @NonNull private MetaData topicMessage;
     @Getter @Setter @NonNull private Throwable exception;
 }
+
+// we can write it easier
+/*@Getter
+@Setter
+@RequiredArgsConstructor
+public class ErrorMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @NonNull
+    private String topicKey;
+
+    @NonNull
+    private MetaData topicMessage;
+
+    @NonNull
+    private Throwable exception;
+}*/
+
+// or we can make it immutable via @Value or records if you are using Java 17+
+
+/*
+@Value
+public class ErrorMessage implements Serializable {
+    String topicKey;
+    MetaData topicMessage;
+    Throwable exception;
+}
+
+public record ErrorMessage(
+        String topicKey,
+        MetaData topicMessage,
+        Throwable exception
+) implements Serializable {}*/

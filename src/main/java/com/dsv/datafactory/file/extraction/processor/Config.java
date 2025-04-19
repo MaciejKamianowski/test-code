e67ff6b74480816324dce517240de4cb3cb344a1
MaciejKamianowski <maciek-kamianowski@o2.pl>
2025-04-19 17:05:01 +0200

    REVIEW - comment 5 - Config

--- src/main/java/com/dsv/datafactory/file/extraction/processor/Config.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/Config.java
@@ -2,12 +2,16 @@ package com.dsv.datafactory.file.extraction.processor;
 
 import com.google.cloud.vision.v1.Feature;
 
+// class name too generic
+// should more descriptive for example KafkaConfig
 public class Config {
 
+	// the fields should be private, final and read only
+	// to make it thread-safe and reliable
 	public String imageExtractionMetadataTopic;
 	public Feature feature;
 
-	public String extractedDocumentTopic;
+	public String extractedDocumentTopic;		// we should have validation for the fields like @NotNull
 
 	public String kafkaClientId;
 
@@ -29,23 +33,25 @@ public class Config {
 
 	public String kafkaTruststorePassword;
 
-	public String kafkaSLLProtocol;
+	public String kafkaSLLProtocol;		// typo kafkaSSLProtocol
 
 	public String kafkaSSLCipher;
 
-	public int kakfaPollIntervalMs;
+	public int kakfaPollIntervalMs;	// typo kafkaPollIntervalMs
 
 	public int kafkaRequestTimeoutMs;
 
-    public String lineServiceUrl;
+    public String lineServiceUrl;		// should be validated @Pattern
 
 	public String startNumberOfClasses;
 
 	public String enableRBAC;
 
-	public String goodnessOfFit;
+	public String goodnessOfFit;		// what exactly goodnessOfFit means in this context
 
     public String googleCredPath;
 
-    public String runGVInPararell;
+    public String runGVInPararell;	// TYPO runGVInParallel
+	// unclear what GV stands for
+	// we should have a Confluence documentation provided what this variable represents
 }

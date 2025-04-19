d6509723d84f9c50c608ca8611ed3958c7a82d6f
MaciejKamianowski <maciek-kamianowski@o2.pl>
2025-04-19 16:55:01 +0200

    REVIEW - comment 4 - Configuration Loader

--- src/main/java/com/dsv/datafactory/file/extraction/processor/util/ConfigurationLoader.java
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/util/ConfigurationLoader.java
@@ -1,10 +1,13 @@
 package com.dsv.datafactory.file.extraction.processor.util;
 
+// the utility class should have private no arguments constructor
 public class ConfigurationLoader {
     public static String getOrDefault(String envName, String defaultValue) {
         return getOrDefault(envName, defaultValue, false);
     }
 
+    // parameter isEnvValueSecretHidden should be primitive so boolean isEnvValueSecretHidden
+    // it may lead to nullpinter execption
     public static String getOrDefault(String envName, String defaultValue, Boolean isEnvValueSecretHidden) {
         String value = System.getenv(envName);
 
@@ -13,9 +16,14 @@ public class ConfigurationLoader {
                 : value;
 
         if (value == null) {
+            // risking exposure sensitive information
+            // instead of sysout use ECSLogger for example
+            // System.out.println("Using environment variable for " + envName);
             System.out.println("Using default value " + defaultValue + " for: " + envName);
             return defaultValue;
         } else {
+            // :  unclear what "or Else" means in this context.
+            // System.out.println("Environment variable " + envName + " not set, using default: " + defaultValue);
             System.out.println(envValueToPrint + " found for: " + envName + " or Else: " + defaultValue);
             return value;
         }
@@ -29,4 +37,14 @@ public class ConfigurationLoader {
             return value;
         }
     }
+
+    // if the class will be used with more than strings we can introduce this method
+/*    public static int getIntOrDefault(String envName, int defaultValue) {
+        String val = System.getenv(envName);
+        try {
+            return val != null ? Integer.parseInt(val) : defaultValue;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Invalid int value for " + envName, e);
+        }
+    }*/
 }
